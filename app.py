1e0344d69715e2817f8042689bd0ef0ecefafc1a
Ashvika <ashvikajain.academia@gmail.com>
2026-02-13 21:36:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -177,8 +177,7 @@ def buildup():
 def popping_question():
     col1, col2, col3 = st.columns([1, 6, 1]) 
     with col2:
-        st.write("he")
-        #st.video("Popping the question.mp4", format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=True, muted=False, width=650)
+        st.video("Popping the question.mp4", format="video/mp4", start_time=0, subtitles=None, end_time=None, loop=False, autoplay=True, muted=False, width=650)
     if st.button("Lemme answer 🫧"):
         st.session_state.page=7
 
